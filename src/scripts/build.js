cc6085dbe014707b4ba4f509e882b6544556e371
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2021-12-25 17:00:31 +0600

    css architecture, node-sass, stylelint-preetirer setup, husky-lint-staged

---
+++ src/scripts/build.js
@@ -0,0 +1,12 @@
+const Fs = require("fs");
+const Path = require("path");
+const Sass = require("node-sass");
+
+const result = Sass.renderSync({
+  data: Fs.renderSync(Path.resolve("src/global.scss")).toString(),
+  outputStyle: "expanded",
+  outFile: "global.css",
+  includePaths: [Path.resolve("src")],
+});
+
+Fs.writeFileSync(Path.resolve("src/lib/global.css"), result.css.toString());
